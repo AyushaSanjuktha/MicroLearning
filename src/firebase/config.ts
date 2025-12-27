dc6092a5e35ed278f7b333b7634c282c4ddae322
Ayushasanjuktha Chekka <ayushasanjuktha@gmail.com>
2025-12-27 14:41:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5339015714-be7c3",
+  "appId": "1:399846723846:web:e2addf27f424315a244e87",
+  "apiKey": "AIzaSyDMZg5hexMpru9iS2c1lWuOwwsejp5-sUs",
+  "authDomain": "studio-5339015714-be7c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "399846723846"
+};
